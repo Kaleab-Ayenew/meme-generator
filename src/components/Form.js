ae940e48f09a12d370bd8770cf83dc286c307623
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 20:40:23 +0300

    regular commit88

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 export default function Form(){
+    function clickHandler(){
+        console.log("The Meme Generator Button was clicked");
+    }
     return (
         <div className='form-container'>
             <form>
                 <input placeholder="Enter Text on Top" className='input-text1'/>
                 <input placeholder='Enter Text at Bottom' className='input-text2'/>
             </form>
-            <button className='generate-meme-button'>Generate Another Meme</button>
+            <button onClick={clickHandler()} className='generate-meme-button'>Generate Another Meme</button>
         </div>
     )
 }
