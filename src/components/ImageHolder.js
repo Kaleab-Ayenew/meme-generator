ecd5fc57a4a89247c769247bca5415f981711ddb
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 16:38:15 +0300

    regular commit21

--- src/components/ImageHolder.js
+++ src/components/ImageHolder.js
@@ -1,1 +1,9 @@
 import React from 'react'
+
+export default function ImageHolder(){
+    return (
+        <div className='image-container'>
+            <img src="some-source" alt="the-image"></img>
+        </div>
+    )
+}
