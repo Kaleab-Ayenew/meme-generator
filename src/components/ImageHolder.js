0a9786d877cb0c5a7c6005755bb53e1756e3394a
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 21:03:53 +0300

    regular commit110

--- src/components/ImageHolder.js
+++ src/components/ImageHolder.js
@@ -8,7 +8,7 @@ export default function ImageHolder(){
 
     return (
         <div className='image-container'>
-            <img onClick={hoverHandler} src={memeImg} alt="the-image"></img>
+            <img onClick={this.hoverHandler} src={memeImg} alt="the-image"></img>
         </div>
     )
 }
