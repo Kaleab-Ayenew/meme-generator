6918bc440fef180bd3e89cb8d846f6391634549e
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 21:59:03 +0300

    regular commit162

--- src/components/PopularMeme.js
+++ src/components/PopularMeme.js
@@ -2,14 +2,16 @@ import React from 'react'
 import MemeCard from './MemeCard'
 import MemeList from './memeList'
 
-export default function PopularMeme(){
+export default function PopularMeme(props){
+    let n = props.ind
     let memeArray = MemeList.data.memes
+    let meme = memeArray[n]
     // let allMemes = memeArray.map((item)=>{
     //     return(<MemeCard data={item} />)
     // } )
     return(
         <div className='meme-cards'>
-            {allMemes}
+            {meme}
         </div>
         
     )
