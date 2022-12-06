1238c556b96ba39046fcdeaf8bc5435a1100c6ed
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 21:57:59 +0300

    regular commit161

--- src/components/PopularMeme.js
+++ src/components/PopularMeme.js
@@ -4,11 +4,9 @@ import MemeList from './memeList'
 
 export default function PopularMeme(){
     let memeArray = MemeList.data.memes
-    let allMemes = memeArray.map((item)=>{
-        return(<MemeCard data={item} />)
-    }
-        
-    )
+    // let allMemes = memeArray.map((item)=>{
+    //     return(<MemeCard data={item} />)
+    // } )
     return(
         <div className='meme-cards'>
             {allMemes}
