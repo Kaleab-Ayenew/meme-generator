4d35698eddd56bd75820739653691cbf56247b7e
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 20:59:39 +0300

    regular commit106

--- src/components/ImageHolder.js
+++ src/components/ImageHolder.js
@@ -2,9 +2,13 @@ import React from 'react'
 import memeImg from '../images/dog-meme.jpg'
 
 export default function ImageHolder(){
+    function hoverHandler(){
+        console.log(this)
+    }
+
     return (
         <div className='image-container'>
-            <img src={memeImg} alt="the-image"></img>
+            <img onClick={hoverHandeler} src={memeImg} alt="the-image"></img>
         </div>
     )
 }
