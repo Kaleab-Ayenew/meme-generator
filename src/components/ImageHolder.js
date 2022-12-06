2d5548b3b649148029e5155b70dbb2e7ea683061
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 17:19:22 +0300

    regular commit60

--- src/components/ImageHolder.js
+++ src/components/ImageHolder.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import memeImg from '../images/dog-meme.jpg'
 
 export default function ImageHolder(){
     return (
         <div className='image-container'>
-            <img src="some-source" alt="the-image"></img>
+            <img src={memeImg} alt="the-image"></img>
         </div>
     )
 }
