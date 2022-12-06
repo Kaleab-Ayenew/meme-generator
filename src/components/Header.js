3aaedcb4c66e288b1645bfc9733d2081e530b0d0
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 16:29:49 +0300

    regular commit13

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,14 @@
 import React from 'react'
+
+export default function Header(){
+    return (
+        <header className='header-container'>
+            <span className='logo-holder'>
+                <img className="logo-img" />
+                <span className='logo-text'>Meme Generator</span>
+            </span>
+            
+
+        </header>
+    )
+}
