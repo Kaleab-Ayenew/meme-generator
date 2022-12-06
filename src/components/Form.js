e95c199a742413f7498834a7a2e8a942558afbdf
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-06 17:31:00 +0300

    regular commit71

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,8 @@ export default function Form(){
     return (
         <div className='form-container'>
             <form>
-                <input className='input-text1'/>
-                <input className='input-text2'/>
+                <input placeholder="Enter Text on Top" className='input-text1'/>
+                <input placeholder='Enter Text at Bottom' className='input-text2'/>
             </form>
             <button className='generate-meme-button'>Generate Another Meme</button>
         </div>
