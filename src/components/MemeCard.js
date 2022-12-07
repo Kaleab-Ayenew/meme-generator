d931a31fc381ce31666450a638b837e07f856c08
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-07 15:41:42 +0300

    regular commit511

--- src/components/MemeCard.js
+++ src/components/MemeCard.js
@@ -5,8 +5,11 @@ export default function MemeCard(props){
     let data = props.data
     return(
         <div className="meme-card-container">
-            <span>
-                {data.name}
+            <span className="upper-text">
+                {data.upperText}
+            </span>
+            <span className="lower-text">
+                {data.lowerText}
             </span>
             <img src={data.url}/>
         </div>
