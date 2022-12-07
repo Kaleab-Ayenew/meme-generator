89419a88449c6d708cea26aaa63a94bc17aaee4e
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-07 12:10:51 +0300

    regular commit378

--- src/components/Box.js
+++ src/components/Box.js
@@ -13,7 +13,7 @@ export default function Box(props){
     let boxComps = boxList.map(
         (item, index)=>{
             let secondClass = item.on ? "box-on" : "box-off"
-            return <div onClick={(event)=>(boxClickHandler(index, event))} className={`box ${secondClass}`}></div>
+            return <div key={item.id} onClick={(event)=>(boxClickHandler(index, event))} className={`box ${secondClass}`}></div>
         }
       )
 
