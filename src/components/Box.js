393788a689608c3677ed76879e489917cc30ce6f
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-07 11:35:57 +0300

    regular commit346

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Box(props){
     let className = props.on ? "box-on" : "box-off";
     return(
-        <div className={`box-container ${secondClass}`}>
-
+        <div className="box-container">
+            <div className={`box ${secondClass}`}>
+                
+            </div>
         </div>
     )
 }
