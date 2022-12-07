4f86609b7bd96a9fd3b4042cee57e8bd59dc0a02
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-07 12:01:16 +0300

    regular commit370

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,10 +4,16 @@ export default function Box(props){
     let boxList = props.boxes
     let setBoxList = props.setBoxes
 
+    function boxClickHandler(ind)=>{
+        let newBoxArray = [...boxList]
+        newBoxArray[ind].on = !newBoxArray[ind].on
+        setBoxList([...boxList, ])
+    }
+
     let boxComps = boxList.map(
-        (item)=>{
+        (item, index)=>{
             let secondClass = item.on ? "box-on" : "box-off"
-            return <div className={`box ${secondClass}`}></div>
+            return <div onClick={} className={`box ${secondClass}`}></div>
         }
       )
 
