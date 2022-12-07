3a638d4779958ec431c828ebd17d0335f2265551
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-07 11:42:17 +0300

    regular commit352

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,18 +3,16 @@ import React from "react";
 export default function Box(props){
     let boxList = props.boxes
     let setBoxList = props.setBoxes
-    
+
     let boxComps = boxList.map(
         (item)=>{
-          return <Box {...item} />
+          return <div className={`box ${secondClass}`}></div>
         }
       )
 
     return(
         <div className="box-container">
-            <div className={`box ${secondClass}`}>
-
-            </div>
+            {boxComps}
         </div>
     )
 }
